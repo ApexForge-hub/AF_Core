d1669cb83af418617c176f1f4101ce7cfa1ed8f2
Alok123kuma <alokkumardos2000@gmail.com>
2026-04-18 00:30:29 +0530

    add main

--- src/componenet/Home.jsx
+++ src/componenet/Home.jsx
@@ -44,7 +44,22 @@ function Home() {
           </form>
       </header>
 
-      <main className="hero"></main>
+       <main className="homeMain">
+      <div className="home-style">Welcome to Apexforge</div>
+
+      <div className="body">
+        <h3 className="goal">Website Goal</h3>
+        <h5>The website should help visitors:</h5>
+        <p></p>
+
+        <ol>
+          <li> Understand your services quickly</li>
+          <li> Trust your expertise</li>
+          <li> Explore solutions</li>
+          <li> Contact you or book a consultation</li>
+        </ol>
+      </div>
+    </main>
 
       <outlet/>
       {/* <Home/> */}
@@ -120,24 +135,7 @@ function Home() {
       </footer>
     </div>
   );
-  // return (
-  //   <main className="homeMain">
-  //     <div className="home-style">Welcome to Apexforge</div>
-
-  //     <div className="body">
-  //       <h3 className="goal">Website Goal</h3>
-  //       <h5>The website should help visitors:</h5>
-  //       <p></p>
-
-  //       <ol>
-  //         <li> Understand your services quickly</li>
-  //         <li> Trust your expertise</li>
-  //         <li> Explore solutions</li>
-  //         <li> Contact you or book a consultation</li>
-  //       </ol>
-  //     </div>
-  //   </main>
-  // );
+ 
 };
 
 export default Home;
