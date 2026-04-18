5e33c806ab0d43a2da25b79d98d2229e9a046f53
Alok123kuma <alokkumardos2000@gmail.com>
2026-04-18 15:09:53 +0530

    import ImageSlider.jsx in HomePage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,11 @@
 import "./HomePage.css";
+import ImageSlider from "./componenet/ImageSlider";
 function HomePage() {
   return (
     <main>
-    <div className="home-page">
-      <h1>Welcome to ApexForge</h1>
+      <ImageSlider />
+      <div className="home-page">
+         <h1>Welcome to ApexForge</h1>
       <p>Your trusted partner in software development and IT solutions.</p>
       <a href="/services" className="cta-button">Explore Our Services</a>
     </div>
