865c56f259cacfefc478f6197b46f7ed249b01c8
Alok123kuma <alokkumardos2000@gmail.com>
2026-04-18 15:52:43 +0530

    add leading slider

--- src/componenet/ImageSlider.jsx
+++ src/componenet/ImageSlider.jsx
@@ -8,25 +8,25 @@ import "./ImageSlider.css";
 function ImageSlider() {
   const slider = [
     {
-      image: "slider1.jpg",
+      image: "/slider1.jpg",
       title: "Empowering Your Digital Journey with ApexForge",
       description:
         "At ApexForge, we are dedicated to empowering your digital journey. With our innovative solutions and expert team, we help you navigate the ever-evolving digital landscape, ensuring your success in the online world.",
     },
     {
-      image: "slider2.jpg",
+      image: "/slider2.jpg",
       title: "Transforming Ideas into Reality with ApexForge",
       description:
         "At ApexForge, we specialize in transforming your ideas into reality. Our team of experts is committed to bringing your vision to life through innovative solutions and cutting-edge technology, ensuring your success in the digital world.",
     },
     {
-      image: "slider3.jpg",
+      image: "/slider3.jpg",
       title: "Innovate, Create, Succeed with ApexForge",
       description:
         "At ApexForge, we believe in the power of innovation to drive success. Our team is dedicated to creating cutting-edge solutions that help you innovate, create, and succeed in the ever-evolving digital landscape.",
     },
     {
-      image: "slider4.jpg",
+      image: "/slider4.jpg",
       title: "Your Success, Our Commitment at ApexForge",
       description:
         "At ApexForge, your success is our commitment. We are dedicated to providing innovative solutions and expert guidance to help you achieve your goals and thrive in the digital world.",
